57cfbbb6a17823fb4715aae2479125b70a424cfb
comodojo <devel@comodojo.org>
2014-07-12 00:20:58 +0200

    tests for conditional routing

---
+++ services/test_route_first.php
@@ -0,0 +1,13 @@
+<?php namespace comodojo\Dispatcher\Service;
+
+class test_route_first extends service {
+	
+	public function get() {
+
+		return "This is the FIRST route";
+
+	}
+
+}
+
+?>
---
+++ services/test_route_second.php
@@ -0,0 +1,13 @@
+<?php namespace comodojo\Dispatcher\Service;
+
+class test_route_second extends service {
+	
+	public function get() {
+
+		return "This is the SECOND route";
+
+	}
+
+}
+
+?>
