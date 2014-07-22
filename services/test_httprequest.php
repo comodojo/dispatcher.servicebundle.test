b23125fc65427c94d39ffb9523d36f0db1df795a
comodojo <info@comodojo.org>
2014-07-22 13:22:03 +0200

    fixed errors in monolog implementation, performer and httprequest test service

--- services/test_httprequest.php
+++ services/test_httprequest.php
@@ -10,13 +10,15 @@ class test_httprequest extends service {
 
     public function get() {
 
+    	$logger = $this->getLogger();
+
         $http = new Httprequest("https://maps.googleapis.com/maps/api/geocode/json?address=Piazza%20del%20Colosseo+Roma");
 
         $return = $http->setPort(443)->get();
 
         $head = $http->getReceivedHeaders();
 
-        $this->logger->debug($head);
+        $logger->debug('Received headers',$head);
 
         return $return;
 
